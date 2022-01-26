c5c744866618c8f351fbf8032a3e81785028aaa2
ChinGray <1745874297@qq.com>
2022-01-26 16:28:01 +0800

    【feat】新增mysql连接配置

---
+++ server/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: '1745874297',
+    database: 'fa_trees',
+    port: '3306'
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 var express = require('express');
 var router = express.Router();
+var models = require('../db') //引入数据库配置
+var mysql = require('mysql') //引入mysql
+
+// 连接数据库
+var conn = mysql.createConnection(models)
+conn.connect()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// conn.end(); //关闭数据库
 
 module.exports = router;
