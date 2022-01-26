73d11e0e7fd770d04e60aa2edcd9716ec6da55b7
ChinGray <1745874297@qq.com>
2022-01-26 16:37:16 +0800

    【feat】新增server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 var app = express();
+
+// 页面路由
+app.use('/api', require('./routes/index'))
+
+//设置跨域访问
+app.all('*', function (req, res, next) {
+  //设置全局的全域问题，我这里是把所有的全部允许了，如果不怕麻烦或者业务需求，你也可以在请求里面写对应的跨域问题
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('X-Powered-By', ' 3.2.1')
+  res.header('Content-Type', 'application/json;charset=utf-8')
+  next()
+})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,16 +37,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
