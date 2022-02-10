6b1e2911f86b1dcf9d36e7dca85109fbd5b99b96
ChinGray <1745874297@qq.com>
2022-02-10 10:08:51 +0800

    【feat】新增server动态菜单

--- src/views/dashboard/Admin/components/PanelGroup.vue
+++ src/views/dashboard/Admin/components/PanelGroup.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row :gutter="40" class="panel-group">
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+    <el-col :xs="12" :sm="12" :lg="8" class="card-panel-col">
       <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
         <div class="card-panel-icon-wrapper icon-people">
           <svg-icon icon-class="peoples" class-name="card-panel-icon" />
@@ -12,7 +12,7 @@
         </div>
       </div>
     </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+    <el-col :xs="12" :sm="12" :lg="8" class="card-panel-col">
       <div class="card-panel" @click="handleSetLineChartData('messages')">
         <div class="card-panel-icon-wrapper icon-message">
           <svg-icon icon-class="message" class-name="card-panel-icon" />
@@ -21,30 +21,6 @@
           <div class="card-panel-text">Messages</div>
           <span class="card-panel-num">81212</span>
           <!--<count-to :start-val="0" :end-val="81212" :duration="3000" class="card-panel-num" />-->
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('purchases')">
-        <div class="card-panel-icon-wrapper icon-money">
-          <svg-icon icon-class="money" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">Purchases</div>
-          <span class="card-panel-num">9280</span>
-          <!--<count-to :start-val="0" :end-val="9280" :duration="3200" class="card-panel-num" />-->
-        </div>
-      </div>
-    </el-col>
-    <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-      <div class="card-panel" @click="handleSetLineChartData('shoppings')">
-        <div class="card-panel-icon-wrapper icon-shopping">
-          <svg-icon icon-class="shopping" class-name="card-panel-icon" />
-        </div>
-        <div class="card-panel-description">
-          <div class="card-panel-text">Shoppings</div>
-          <span class="card-panel-num">13600</span>
-          <!-- <CountTo :start-val="0" :end-val="13600" class="card-panel-num" />-->
         </div>
       </div>
     </el-col>
