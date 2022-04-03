dd3fcab0aa217234406dd4cdb203fd9ec92c4a7d
chinray <1745874297@qq.com>
2022-04-03 21:15:36 +0800

    【feat】编号布局改变

--- server/routes/user/sql.js
+++ server/routes/user/sql.js
@@ -1,11 +1,11 @@
 // SQL语句封裝
 var userSqls = {
     login: 'SELECT id,username FROM user WHERE username=? and password=?',
-    getUserInfo: 'SELECT username,avatar,roles,tag FROM user WHERE id=?',
-    getUsers: 'SELECT id,username,avatar,roles,tag FROM user',
-    getUsersByName: 'SELECT id,username,avatar,roles,tag FROM user WHERE username=?',
-    getUsersByRoles: 'SELECT id,username,avatar,roles,tag FROM user WHERE roles=?',
-    getUsersByNR: 'SELECT id,username,avatar,roles,tag FROM user WHERE username=? and roles=?',
+    getUserInfo: 'SELECT username,avatar,roles,tag,create_time,update_time FROM user WHERE id=?',
+    getUsers: 'SELECT id,username,avatar,roles,tag,create_time,update_time FROM user',
+    getUsersByName: 'SELECT id,username,avatar,roles,tag,create_time,update_time FROM user WHERE username=?',
+    getUsersByRoles: 'SELECT id,username,avatar,roles,tag,create_time,update_time FROM user WHERE roles=?',
+    getUsersByNR: 'SELECT id,username,avatar,roles,tag,create_time,update_time FROM user WHERE username=? and roles=?',
     addUser: 'INSERT INTO user(username, password, avatar, roles, tag) VALUES(?,?,?,?,?)',
     updateUser: 'UPDATE user SET username=?, avatar=?, roles=?, tag=? WHERE id=?',
     deleteUser: 'DELETE FROM user WHERE id=?'
