0db9371c033c0ac95dbcbf1b4c49a4f4a02df941
chinray <1745874297@qq.com>
2022-04-11 08:04:26 +0800

    【feat】修改初始密码

--- server/routes/user/config.js
+++ server/routes/user/config.js
@@ -122,7 +122,7 @@ var userControll = {
         })
     },
     addUser: function (req, res, next) {
-        req.body.password = `fa123456`
+        req.body.password = `123456`
         switch (req.body.roles) {
             case 'admin': req.body.tag = 'danger'; break;
             case 'worker': req.body.tag = 'warning'; break;
