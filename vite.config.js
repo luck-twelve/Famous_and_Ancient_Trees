b35c2000da49fd92b20378f78994733e0a06e507
chinray <1745874297@qq.com>
2022-04-24 09:52:16 +0800

    【feat】页面调整

--- vite.config.js
+++ vite.config.js
@@ -93,7 +93,6 @@ export default ({ command, mode }) => {
           drop_debugger: true
         }
       },
-
       //build assets Separate
       assetsDir: 'static/assets',
       rollupOptions: {
