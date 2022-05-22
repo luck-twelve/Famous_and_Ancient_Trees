8089de501d990d37d12e8c20bb50d8256fc8ac6c
chinray <1745874297@qq.com>
2022-05-22 12:33:04 +0800

    【feat】页面调整

--- server/routes/functions.js
+++ server/routes/functions.js
@@ -172,6 +172,7 @@ const actions = {
         var lastMonthToday = new Date(
             new Date().getTime() - 30 * 24 * 60 * 60 * 1000
         );
+
         var lastMonthYear = lastMonthToday.getFullYear();
         var lastMonth = lastMonthToday.getMonth() + 1;
         var lastMonthDay =
